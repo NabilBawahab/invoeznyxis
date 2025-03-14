0bf503737dc4f096d897e08e263744eb3e1b56c6
Chromaticism <rifqi.124140200@student.itera.ac.id>
2025-03-14 20:06:06 +0700

    feat/edit-invoice

--- src/app/(app)/dashboard/history/[id]/page.js
+++ src/app/(app)/dashboard/history/[id]/page.js
@@ -1,3 +1,32 @@
-export default function Page() {
-  return <div>ini page individual history invoice</div>;
+import { auth } from "@/libs/auth";
+import UpdateInvoiceForm from "./_components/update-invoice-form";
+import { prisma } from "@/utils/prisma";
+
+export default async function Page({ params }) {
+  const session = await auth();
+
+  const { id } = await params;
+
+  const invoice = await prisma.invoice.findUnique({
+    where: { id },
+  });
+
+  const items = await prisma.item.findMany({
+    where: { invoiceId: invoice.id },
+  });
+
+  const serializedItems = items.map((item) => ({
+    ...item,
+    price: item.price.toString(),
+  }));
+
+  return (
+    <main className="w-full max-h-screen space-y-4">
+      <UpdateInvoiceForm
+        session={session}
+        invoice={invoice}
+        items={serializedItems}
+      />
+    </main>
+  );
 }
