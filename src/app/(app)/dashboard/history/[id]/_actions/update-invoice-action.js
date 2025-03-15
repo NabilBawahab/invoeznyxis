2d70d4cd1e26210091161e8562d3c3823cf67730
Chromaticism <rifqi.124140200@student.itera.ac.id>
2025-03-16 00:49:32 +0700

    fix/some-fixes

--- src/app/(app)/dashboard/history/[id]/_actions/update-invoice-action.js
+++ src/app/(app)/dashboard/history/[id]/_actions/update-invoice-action.js
@@ -1,43 +1,168 @@
 "use server";
 
 import { prisma } from "@/utils/prisma";
+import { openai } from "@/utils/openai";
+import { redirect } from "next/navigation";
+import { resend } from "@/utils/resend";
+import path from "path";
+import fs from "fs";
 
 export async function updateInvoiceAction(_, formData) {
-  const name = formData.name;
-  const dueDate = formData.dueDate;
-  const recipient = formData.recipient;
-  const recipientEmail = formData.recipientEmail;
-  const recipientPhone = formData.recipientPhone;
-  const recipientAddress = formData.recipientAddress;
-  const authorId = formData.authorId;
+  let name = formData.name;
+  const userName = formData.userName;
+  let dueDate = formData.dueDate;
+  let recipient = formData.recipient;
+  let recipientEmail = formData.recipientEmail;
+  let recipientPhone = formData.recipientPhone;
+  let recipientAddress = formData.recipientAddress;
+  let authorId = formData.authorId;
   const invoiceItems = formData.invoiceItems;
   const invoiceId = formData.id;
-  try {
-    const newInvoice = await prisma.invoice.update({
-      where: { id: invoiceId },
-      data: {
-        name,
-        dueDate,
-        recipient,
-        recipientEmail,
-        recipientPhone,
-        recipientAddress,
-        authorId,
-      },
+  const invoiceType = formData.invoiceType;
+  const organization = formData.organization;
+  const senderEmail = formData.senderEmail;
+  const totalPrice = formData.totalPrice;
+  const type = formData.type; // type submit/preview
+
+  let newInvoice;
+
+  if (type === "generate") {
+    const completion = await openai.chat.completions.create({
+      model: "deepseek/deepseek-chat:free",
+      messages: [
+        {
+          role: "system",
+          content: `You are the secretary/personal assistant of this user:
+          User:
+          name: ${userName}
+          organization/company: ${organization}
+          Email: ${senderEmail}
+
+          Currently, you are ordered by user to make a ${invoiceType} invoice cover letter to their client, with the following information:
+
+          Recipient:
+          organization/company: ${recipient}
+          Address: ${recipientAddress}
+          phone number: ${recipientPhone}
+          recipient email: ${recipientEmail}
+          item: ${invoiceItems.map((item) => {
+            return `${item.name} - item price :${item.price}`;
+          })}
+          quantity: ${invoiceItems.map((item) => {
+            return `${item.quantity}`;
+          })}
+          total prices: ${totalPrice},
+          bill due date: ${dueDate}
+
+          The results should be ready to use and will be sent to the client email
+
+          IMPORTANT: JUST A LETTER, NO EXPLANATIONS AND KEY IMPROVEMENTS, NO USER PHONE NUMBER, ALL CURRENCY IN INDONESIAN RUPIAH, AND RESPONSE ARE IN ENGLISH
+
+          OUPUT MUST BE IN VALID JSON FORMAT AND READY TO USE WITHOUT EDIT, WITHOUT ANY BACKTICKS (\`) AND WITHOUT '''JSON
+
+          Example:
+            {"subject":"email subject", "letter": "content you make(cover letter)"}
+          `,
+        },
+
+        {
+          role: "user",
+          content: `I want to give an invoice to my client, please make a cover letter for the subject and body of the email, i want an ${invoiceType} cover letter.`,
+        },
+      ],
+      temperature: 0.7,
     });
-    await prisma.item.deleteMany({ where: { invoiceId } });
-    await prisma.item.createMany({
-      data: invoiceItems.map((item) => ({
-        ...item,
-        invoiceId: newInvoice.id,
-        price: Number(item.price),
-        quantity: Number(item.quantity),
-      })),
-    });
-  } catch (error) {
-    console.log({
-      message: "Error updating invoice or invoice item",
-      error: error,
-    });
+    const result = completion.choices[0].message.content;
+
+    try {
+      const parsedResult = JSON.parse(result);
+
+      return parsedResult;
+    } catch (error) {
+      console.log({
+        message: "Error parsing JSON",
+        error: error,
+      });
+    }
   }
+  if (type === "submit") {
+    const subject = formData.subject;
+    const letter = formData.letter;
+
+    try {
+      newInvoice = await prisma.invoice.update({
+        where: { id: invoiceId },
+        data: {
+          name,
+          dueDate,
+          recipient,
+          recipientEmail,
+          recipientPhone,
+          recipientAddress,
+          authorId,
+        },
+      });
+      await prisma.item.deleteMany({ where: { invoiceId } });
+      await prisma.item.createMany({
+        data: invoiceItems.map((item) => ({
+          ...item,
+          invoiceId: newInvoice.id,
+          price: Number(item.price),
+          quantity: Number(item.quantity),
+        })),
+      });
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: authorId,
+        },
+      });
+
+      const filePath = path.join(
+        process.cwd(),
+        "src",
+        "components",
+        "invoice-email-template.html"
+      );
+
+      let html = fs.readFileSync(filePath, "utf8");
+
+      html = html.replace("{{recipient}}", recipient);
+      html = html.replace("{{recipientEmail}}", recipientEmail);
+      html = html.replace("{{recipientAddress}}", recipientAddress);
+      html = html.replace("{{recipientPhone}}", recipientPhone);
+      html = html.replace("{{userName}}", userName);
+      html = html.replace("{{senderEmail}}", senderEmail);
+      html = html.replace("{{dueDate}}", dueDate);
+      html = html.replace("{{totalPrice}}", totalPrice);
+      html = html.replace("{{letter}}", letter);
+      html = html.replace(
+        "{{invoiceItems}}",
+        invoiceItems.map((item) => {
+          return `<tr>
+          <td>${item.name}</td>
+          <td>${item.description}</td>
+          <td>Rp${item.price}</td>
+          <td>${item.quantity}</td>
+          <td>Rp${item.price * item.quantity}</td>
+        </tr>`;
+        })
+      );
+
+      await resend.emails.send({
+        from: "InvoEzz <no-reply@invoezz.com>",
+        to: [recipientEmail],
+        subject: subject,
+        text: letter,
+        html: html,
+      });
+    } catch (error) {
+      console.log({
+        message: "Error updating invoice or invoice item",
+        error: error,
+      });
+    }
+  }
+
+  redirect(`/dashboard/history/${newInvoice.id}`);
 }
--- src/app/(app)/dashboard/history/[id]/_components/update-invoice-form.jsx
+++ src/app/(app)/dashboard/history/[id]/_components/update-invoice-form.jsx
@@ -4,15 +4,24 @@ import {
   Card,
   DatePicker,
   Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Select,
+  SelectItem,
   Table,
   TableBody,
   TableCell,
   TableColumn,
   TableHeader,
   TableRow,
+  Textarea,
+  useDisclosure,
 } from "@heroui/react";
 import { FaPlus, FaTrash } from "react-icons/fa";
-import React, { useActionState } from "react";
+import React, { useActionState, useState } from "react";
 import { startTransition } from "react";
 import { format } from "date-fns";
 import { parseDate } from "@internationalized/date";
@@ -24,7 +33,12 @@ const RupiahIcon = () => {
 };
 
 export default function UpdateInvoiceForm({ session, invoice, items }) {
-  const [_, formAction, pending] = useActionState(updateInvoiceAction, null);
+  const [state, formAction, pending] = useActionState(
+    updateInvoiceAction,
+    null
+  );
+
+  const [type, setType] = useState("");
 
   const { control, register, formState, handleSubmit, setValue } = useForm({
     defaultValues: {
@@ -36,6 +50,7 @@ export default function UpdateInvoiceForm({ session, invoice, items }) {
       recipientAddress: invoice.recipientAddress,
       authorId: session.user.id,
       invoiceItems: items,
+      invoiceType: "",
     },
   });
   const { fields, append, remove } = useFieldArray({
@@ -81,219 +96,312 @@ export default function UpdateInvoiceForm({ session, invoice, items }) {
     });
   };
 
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
   return (
     <form onSubmit={handleSubmit(submitFormAction)} className="w-5/6 mx-auto">
-      <Card className="grid grid-rows-1 grid-cols-12 w-full space-y-2 shadow-md p-4">
-        <div className="col-span-12 ">
-          <div className="space-y-2">
-            <input hidden defaultValue={invoice.id} {...register("id")}></input>
-            <Input
-              variant="flat"
-              radius="sm"
-              size="lg"
-              label="What is this?"
-              defaultValue="Invoice"
-              isRequired
-              {...register("name")}
-            />
-
-            <div className="flex justify-end mb-5">
-              <p>{format(new Date(), "PPP")}</p>
-            </div>
-          </div>
-        </div>
-        <div className="col-span-8 grid grid-cols-8 grid-rows-1">
-          <div className="col-span-4">
-            <Input
-              type="text"
-              variant="flat"
-              radius="sm"
-              size="lg"
-              label="Bill to?"
-              isRequired
-              {...register("recipient")}
-            />
-          </div>
-        </div>
-        <div className="grid col-span-4 grid-cols-4">
-          <div className="col-span-4">
-            <Input
-              type="text"
-              variant="flat"
-              radius="sm"
-              size="lg"
-              label="Who is this bill from?"
-              isDisabled
-              defaultValue={session.user.username}
-            />
-          </div>
-        </div>
-        <div className="grid col-span-8 grid-cols-8">
-          <div className="col-span-4">
-            <Input
-              type="email"
-              variant="flat"
-              radius="sm"
-              size="sm"
-              label="receiver@contact.com"
-              isRequired
-              {...register("recipientEmail")}
-            />
-          </div>
-        </div>
-        <div className="col-span-4 grid grid-cols-4 grid-rows-1">
-          <div className="col-span-4">
-            <Input
-              type="email"
-              variant="flat"
-              radius="sm"
-              size="sm"
-              label="sender@contact.com"
-              isDisabled
-              defaultValue={session.user.email}
-            />
-          </div>
-        </div>
-        <div className="col-span-12 flex justify-between">
-          <div>
-            <div className="flex flex-row gap-x-2">
-              <Input
-                type="text"
-                variant="flat"
-                radius="sm"
-                size="sm"
-                label="Recipient address"
-                {...register("recipientAddress")}
-                isRequired
-              />
-              <Input
-                type="text"
-                variant="flat"
-                radius="sm"
-                size="sm"
-                label="Recipient phone"
-                {...register("recipientPhone")}
-                isRequired
-              />
-            </div>
-          </div>
-          <DatePicker
-            isRequired
-            onChange={(date) =>
-              setValue("dueDate", new Date(date.year, date.month - 1, date.day))
-            }
-            className="max-w-[300px]"
-            label="Invoice due date"
-            defaultValue={parseDate(
-              invoice.dueDate.toISOString().split("T")[0]
-            )}
-          />
-        </div>
-
-        <Table
-          isVirtualized
-          maxTableHeight={250}
-          aria-label="Invoice Item"
-          radius="none"
-          topContentPlacement="outside"
-          shadow="none"
-          classNames={{
-            td: "gap-x-10",
-            wrapper: "bg-inherit",
-          }}
-          className="col-span-12 border"
-        >
-          <TableHeader>
-            <TableColumn className="w-1/4">Item</TableColumn>
-            <TableColumn className="w-1/4">Description</TableColumn>
-            <TableColumn className="w-1/6">Price</TableColumn>
-            <TableColumn className="w-1/6">Quantity</TableColumn>
-            <TableColumn className="w-1/12">Action</TableColumn>
-          </TableHeader>
-          <TableBody>
-            {fields.map((field, index) => {
-              return (
-                <TableRow key={field.id}>
-                  <TableCell className="ps-0">
-                    <Input
-                      isRequired
-                      variant="flat"
-                      radius="sm"
-                      placeholder="item name"
-                      {...register(`invoiceItems.${index}.name`)}
-                    />
-                  </TableCell>
-                  <TableCell className="ps-0">
-                    <Input
-                      isRequired
-                      variant="flat"
-                      radius="sm"
-                      placeholder="item description"
-                      {...register(`invoiceItems.${index}.description`)}
-                    />
-                  </TableCell>
-                  <TableCell className="ps-0">
-                    <Input
-                      min={1}
-                      type="number"
-                      variant="flat"
-                      radius="sm"
-                      startContent={<RupiahIcon />}
-                      {...register(`invoiceItems.${index}.price`)}
-                    />
-                  </TableCell>
-                  <TableCell className="ps-0">
-                    <Input
-                      min={1}
-                      type="number"
-                      variant="flat"
-                      radius="sm"
-                      placeholder="item quantity"
-                      {...register(`invoiceItems.${index}.quantity`)}
-                    />
-                  </TableCell>
-                  <TableCell className="px-0">
-                    <Button onPress={() => remove(index)} color="danger">
-                      {" "}
-                      <FaTrash />{" "}
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
-        <Button
-          onPress={() =>
-            append({
-              name: "",
-              description: "",
-              price: 1000,
-              quantity: 1,
-            })
-          }
-          startContent={<FaPlus />}
-          color="secondary"
-          variant="light"
-        >
-          Add Item
-        </Button>
-
-        <div className="col-span-12 grid grid-cols-4">
-          <SubTotal />
-        </div>
-      </Card>
       <div>
-        <p className="text-danger-500 text-sm">
-          {formState.errors?.invoiceItems?.root?.message}
-        </p>
+        <Card className="grid grid-rows-1 grid-cols-12 w-full space-y-2 shadow-md p-4">
+          <div className="col-span-12 ">
+            <div className="space-y-2">
+              <input
+                hidden
+                defaultValue={invoice.id}
+                {...register("id")}
+              ></input>
+              <Input
+                variant="flat"
+                radius="sm"
+                size="lg"
+                label="What is this?"
+                defaultValue="Invoice"
+                isRequired
+                {...register("name")}
+              />
+
+              <div className="flex justify-end mb-5">
+                <p>{format(new Date(), "PPP")}</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-8 grid grid-cols-8 grid-rows-1">
+            <div className="col-span-4">
+              <Input
+                type="text"
+                variant="flat"
+                radius="sm"
+                size="lg"
+                label="Bill to?"
+                isRequired
+                {...register("recipient")}
+              />
+            </div>
+          </div>
+          <div className="grid col-span-4 grid-cols-4">
+            <div className="col-span-4">
+              <Input
+                type="text"
+                variant="flat"
+                radius="sm"
+                size="lg"
+                label="Who is this bill from?"
+                isDisabled
+                defaultValue={session.user.username}
+              />
+            </div>
+          </div>
+          <div className="grid col-span-8 grid-cols-8">
+            <div className="col-span-4">
+              <Input
+                type="email"
+                variant="flat"
+                radius="sm"
+                size="sm"
+                label="receiver@contact.com"
+                isRequired
+                {...register("recipientEmail")}
+              />
+            </div>
+          </div>
+          <div className="col-span-4 grid grid-cols-4 grid-rows-1">
+            <div className="col-span-4">
+              <Input
+                type="email"
+                variant="flat"
+                radius="sm"
+                size="sm"
+                label="sender@contact.com"
+                isDisabled
+                defaultValue={session.user.email}
+              />
+            </div>
+          </div>
+          <div className="col-span-12 flex justify-between">
+            <div>
+              <div className="flex flex-row gap-x-2">
+                <Input
+                  type="text"
+                  variant="flat"
+                  radius="sm"
+                  size="sm"
+                  label="Recipient address"
+                  {...register("recipientAddress")}
+                  isRequired
+                />
+                <Input
+                  type="text"
+                  variant="flat"
+                  radius="sm"
+                  size="sm"
+                  label="Recipient phone"
+                  {...register("recipientPhone")}
+                  isRequired
+                />
+              </div>
+            </div>
+            <DatePicker
+              isRequired
+              onChange={(date) =>
+                setValue(
+                  "dueDate",
+                  new Date(date.year, date.month - 1, date.day)
+                )
+              }
+              className="max-w-[300px]"
+              label="Invoice due date"
+              defaultValue={parseDate(
+                invoice.dueDate.toISOString().split("T")[0]
+              )}
+            />
+          </div>
+
+          <Table
+            isVirtualized
+            maxTableHeight={250}
+            aria-label="Invoice Item"
+            radius="none"
+            topContentPlacement="outside"
+            shadow="none"
+            classNames={{
+              td: "gap-x-10",
+              wrapper: "bg-inherit",
+            }}
+            className="col-span-12 border"
+          >
+            <TableHeader>
+              <TableColumn className="w-1/4">Item</TableColumn>
+              <TableColumn className="w-1/4">Description</TableColumn>
+              <TableColumn className="w-1/6">Price</TableColumn>
+              <TableColumn className="w-1/6">Quantity</TableColumn>
+              <TableColumn className="w-1/12">Action</TableColumn>
+            </TableHeader>
+            <TableBody>
+              {fields.map((field, index) => {
+                return (
+                  <TableRow key={field.id}>
+                    <TableCell className="ps-0">
+                      <Input
+                        isRequired
+                        variant="flat"
+                        radius="sm"
+                        placeholder="item name"
+                        {...register(`invoiceItems.${index}.name`)}
+                      />
+                    </TableCell>
+                    <TableCell className="ps-0">
+                      <Input
+                        isRequired
+                        variant="flat"
+                        radius="sm"
+                        placeholder="item description"
+                        {...register(`invoiceItems.${index}.description`)}
+                      />
+                    </TableCell>
+                    <TableCell className="ps-0">
+                      <Input
+                        min={1}
+                        type="number"
+                        variant="flat"
+                        radius="sm"
+                        startContent={<RupiahIcon />}
+                        {...register(`invoiceItems.${index}.price`)}
+                      />
+                    </TableCell>
+                    <TableCell className="ps-0">
+                      <Input
+                        min={1}
+                        type="number"
+                        variant="flat"
+                        radius="sm"
+                        placeholder="item quantity"
+                        {...register(`invoiceItems.${index}.quantity`)}
+                      />
+                    </TableCell>
+                    <TableCell className="px-0">
+                      <Button onPress={() => remove(index)} color="danger">
+                        {" "}
+                        <FaTrash />{" "}
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+          <Button
+            onPress={() =>
+              append({
+                name: "",
+                description: "",
+                price: 1000,
+                quantity: 1,
+              })
+            }
+            startContent={<FaPlus />}
+            color="secondary"
+            variant="light"
+          >
+            Add Item
+          </Button>
+
+          <div className="col-span-12 grid grid-cols-4">
+            <SubTotal />
+          </div>
+        </Card>
+        <div>
+          <p className="text-danger-500 text-sm">
+            {formState.errors?.invoiceItems?.root?.message}
+          </p>
+        </div>
+      </div>
+      <p className="text-sm text-gray-500">Create cover letter with AI</p>
+      <Select label="Select prompt" {...register("invoiceType")}>
+        <SelectItem key="Professional and Firm">
+          Professional and Firm
+        </SelectItem>
+        <SelectItem key="Professional and Friendly">
+          Professional and Friendly
+        </SelectItem>
+        <SelectItem key="Professional and Detailed">
+          Professional and Detailed
+        </SelectItem>
+        <SelectItem key=" Professional, Concise and Clear">
+          Professional, Concise and Clear
+        </SelectItem>
+      </Select>
+      <input {...register("type")} value={type} hidden />
+      <input
+        {...register("userOrganization")}
+        defaultValue={session.user.organization}
+        hidden
+      />
+      <div className="lg:flex justify-between items-center">
+        <h3 className="font-bold text-xs  text-red-500">
+          AI can make mistake, please check before proceed!
+        </h3>
+        <div className="space-x-2">
+          <Button
+            color="secondary"
+            variant="ghost"
+            type="button"
+            radius="full"
+            size="sm"
+            onPress={() => {
+              if (!state?.letter || !state?.subject) {
+                return alert("Please generate the cover letter first");
+              }
+              onOpen();
+            }}
+          >
+            Preview
+          </Button>
+          <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+            <ModalContent>
+              {(onClose) => (
+                <div className="p-2">
+                  <ModalHeader>
+                    <Input
+                      label="Email Subject"
+                      defaultValue={state?.subject}
+                      {...register("subject")}
+                    />
+                  </ModalHeader>
+                  <ModalBody>
+                    <Textarea
+                      label="Cover Letter"
+                      defaultValue={state?.letter}
+                      {...register("letter")}
+                    />
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button color="danger" variant="light" onPress={onClose}>
+                      Close
+                    </Button>
+                  </ModalFooter>
+                </div>
+              )}
+            </ModalContent>
+          </Modal>
+          <Button
+            type="submit"
+            onPress={() => {
+              setValue("type", "generate");
+            }}
+            isLoading={pending}
+            size="sm"
+            radius="full"
+            className="bg-gradient-to-tr from-cyan-500 to-blue-500 text-white shadow-lg font-bold"
+          >
+            Save & Generate
+          </Button>
+        </div>
       </div>
       <Button
         type="submit"
         fullWidth
         className="mt-3 rounded-lg bg-blue-500 text-white"
       >
-        Update Invoice
+        Update & Resend Invoice
       </Button>
     </form>
   );
