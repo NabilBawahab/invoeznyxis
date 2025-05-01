eef66a751173df26fc19b3ea0633b3359dc7c031
Nabil Muhammad Bawahab <nabilbawahab@gmail.com>
2025-05-01 17:58:43 +0700

    add pagination to history pages

--- src/app/(app)/dashboard/history/_components/invoice_table.jsx
+++ src/app/(app)/dashboard/history/_components/invoice_table.jsx
@@ -3,6 +3,7 @@
 import {
   Button,
   Form,
+  Pagination,
   Table,
   TableBody,
   TableCell,
@@ -12,48 +13,84 @@ import {
 } from "@heroui/react";
 import { deleteInvoice } from "../action";
 import Link from "next/link";
-import { useActionState } from "react";
+import { useActionState, useState } from "react";
 
 export function InvoiceTable({ invoices: invoices }) {
   const [state, formAction, pending] = useActionState(deleteInvoice, null);
 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const itemPerPage = 10;
+
+  // example: item di database 10, lalu item per page yang kita mau adalah 10, maka pagesnya nanti cuman 1
+  // math ceil ini pembulatan, misal 3/2 = 1,5. Namun di math ceil hasilnya jadi 2
+  const totalPages = Math.ceil(invoices.length / itemPerPage);
+
+  const paginatedInvoices = invoices.slice(
+    (currentPage - 1) * itemPerPage, // index yang mau diambil mulai dari berapa
+    currentPage * itemPerPage, // index akhirnya
+  );
+
   return (
-    <Table removeWrapper className="overflow-auto">
-      <TableHeader>
-        <TableColumn>Name</TableColumn>
-        <TableColumn>Recipient</TableColumn>
-        <TableColumn>Address</TableColumn>
-        <TableColumn className="w-0">Action</TableColumn>
-      </TableHeader>
-      <TableBody emptyContent={"No invoices"}>
-        {invoices.map((invoice) => {
-          return (
-            <TableRow key={invoice.id}>
-              <TableCell>{invoice.name}</TableCell>
-              <TableCell>{invoice.recipient}</TableCell>
-              <TableCell>{invoice.recipientAddress}</TableCell>
-              <TableCell>
-                <div className="flex justify-between space-x-2">
-                  <Link href={`/dashboard/history/${invoice.id}`}>
-                    <Button size="sm">Edit</Button>
-                  </Link>
-                  <Form action={formAction}>
-                    <input name="id" defaultValue={invoice.id} hidden />
-                    <Button
-                      className="bg-red-500 text-white"
-                      size="sm"
-                      type="submit"
-                      isLoading={pending}
-                    >
-                      Delete
-                    </Button>
-                  </Form>
-                </div>
-              </TableCell>
-            </TableRow>
-          );
-        })}
-      </TableBody>
-    </Table>
+    <>
+      <Table removeWrapper className="overflow-auto min-h-[400px]">
+        <TableHeader>
+          <TableColumn>Name</TableColumn>
+          <TableColumn>Recipient</TableColumn>
+          <TableColumn>Address</TableColumn>
+          <TableColumn className="w-0">Action</TableColumn>
+        </TableHeader>
+        <TableBody emptyContent={"No invoices"}>
+          {paginatedInvoices.map((invoice) => {
+            return (
+              <TableRow key={invoice.id}>
+                <TableCell>{invoice.name}</TableCell>
+                <TableCell>{invoice.recipient}</TableCell>
+                <TableCell>{invoice.recipientAddress}</TableCell>
+                <TableCell>
+                  <div className="flex justify-between space-x-2">
+                    <Link href={`/dashboard/history/${invoice.id}`}>
+                      <Button size="sm">Edit</Button>
+                    </Link>
+                    <Form action={formAction}>
+                      <input name="id" defaultValue={invoice.id} hidden />
+                      <Button
+                        className="bg-red-500 text-white"
+                        size="sm"
+                        type="submit"
+                        isLoading={pending}
+                      >
+                        Delete
+                      </Button>
+                    </Form>
+                  </div>
+                </TableCell>
+              </TableRow>
+            );
+          })}
+          {Array.from({ length: itemPerPage - paginatedInvoices.length }).map(
+            (_, index) => {
+              return (
+                <TableRow key={index}>
+                  <TableCell className="h-12" />
+                  <TableCell className="h-12" />
+                  <TableCell className="h-12" />
+                  <TableCell className="h-12" />
+                </TableRow>
+              );
+            },
+          )}
+        </TableBody>
+      </Table>
+      {totalPages > 1 && (
+        <Pagination
+          initialPage={currentPage}
+          total={2}
+          onChange={setCurrentPage}
+          isDisabled={pending}
+          className="flex justify-center"
+        />
+      )}
+    </>
   );
 }
